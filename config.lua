a572a02fffde34429ce6390f6100dd8aab1dc13c
ItzKizar <99242342+ItzKizar@users.noreply.github.com>
2022-06-07 20:10:32 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,5 @@
 Config = {}
 
--- Config.GaragePedlocation = {
---     {cords = vector3(-1178.588, -891.9196, 13.799622 -1), h = 302.58825}  -- Garage NPC Spawn Location.
--- }
 
 
 Config.Items = {
@@ -10,36 +7,44 @@ label = "Shop",
     slots = 7,
     items = {
         [1] = {
-            name = "cakemix",  --bun
+            name = "cakemix",  
             price = 0,
-            amount = 5,
+            amount = 50,
             info = {},
             type = "item",
             slot = 1,
         },
         [2] = {
-            name = "cookiemix",   --raw
+            name = "cookiemix",   
             price = 0,
-            amount = 5,
+            amount = 50,
             info = {},
             type = "item",
             slot = 2,
         },
         [3] = {
-            name = "donutmix", --tomato
+            name = "donutmix", 
             price = 0,
-            amount = 5,
+            amount = 50,
             info = {},
             type = "item",
             slot = 3,
         },
         [4] = {
-            name = "icecreammix",  --lettuce
+            name = "smoothiemix",  
             price = 0,
-            amount = 5,
+            amount = 50,
             info = {},
             type = "item",
             slot = 4,
+        },
+        [5] = {
+            name = "strawberry",  
+            price = 0,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 5,
         },
     }
 }
