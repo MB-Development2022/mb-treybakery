14737ed0c0c146773763b9531e66138b1d5437b4
Tom <sextontom0000@gmail.com>
2022-06-13 17:46:30 -0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -67,20 +67,23 @@ AddEventHandler("mb-treybakery:client:craftstation", function(data)
             LoadAnimDict(data.dictionary)
             TaskPlayAnim(PlayerPedId(), data.dictionary, data.animname, 6.0, -6.0, -1, 46, 0, 0, 0, 0)
             FreezeEntityPosition(PlayerPedId(), true)
+            print(data.progressbar)
+            print(data.progresstime)
             QBCore.Functions.Progressbar("pickup_sla", data.progressbar, data.progresstime, false, true, {
                 disableMovement = true,
                 disableCarMovement = false,
                 disableMouse = false,
                 disableCombat = false,
             }, {}, {}, {}, function() -- Done
-                Working = false
                 TriggerEvent('inventory:client:busy:status', false)
                 ClearPedTasksImmediately(PlayerPedId())
                 FreezeEntityPosition(PlayerPedId(), false)
+                print(data.amount)
                 TriggerServerEvent('mb-treybakery:server:craft', data.required, data.item, data.amount)
             end, function()
                 TriggerEvent('inventory:client:busy:status', false)
-                Working = false
+                ClearPedTasksImmediately(PlayerPedId())
+                FreezeEntityPosition(PlayerPedId(), false)
             end, data.item)   
         else
             QBCore.Functions.Notify("You're missing ingredients!", "error")
@@ -100,10 +103,17 @@ RegisterNetEvent("mb-treybakery:client:storage", function(data)
 end)
 
 -- Crafting Menus
-RegisterNetEvent("mb-treybakery:client:openmenu", function(Station)
-    local MBTreyMenu = {}
+RegisterNetEvent("mb-treybakery:client:openmenu", function(Station, Name)
+    local MBTreyMenu = {{ header = Name, isMenuHeader = true }}
+    MBTreyMenu[#MBTreyMenu+1] = {
+        header = "⬅ Close Menu",
+        txt = "",
+        params = {
+            event = "qb-menu:client:closeMenu"
+        }
+    }
     for k, v in pairs(Station) do
-        table.insert(MBTreyMenu, {
+        MBTreyMenu[#MBTreyMenu+1] = {
             header = "<img src=nui://qb-inventory/html/images/"..QBCore.Shared.Items[v.item].image.." width=30px>".. QBCore.Shared.Items[v.item].label,
             txt = "-".. v.txt,
             params = {
@@ -111,16 +121,16 @@ RegisterNetEvent("mb-treybakery:client:openmenu", function(Station)
                 args = {
                     item = v.item,
                     required = v.required,
-                    progressbar = v.pbmsg, 
-                    progresstime = v.pbtime,
+                    progressbar = v.progressMsg, 
+                    progresstime = v.progressTime,
                     dictionary = v.animDict,
                     animname = v.anim,
                     amount = v.amount
                 }
-            },
-        })
+            }
+        }
     end
-    exports['qb-menu']:openMenu(MBTreyMenu) 
+        exports['qb-menu']:openMenu(MBTreyMenu)
 end)
 
 -- Shops
@@ -194,4 +204,30 @@ RegisterNetEvent('mb-treybakery:client:ordermenu', function()
             }
         },
     })
+end)
+
+RegisterNetEvent("mb-trey:WeddingCakeFull", function()
+    TriggerServerEvent('QBCore:Server:RemoveItem', "weddingcakefull", 1)
+    TriggerServerEvent('QBCore:Server:AddItem', "weddingcake", 12)
+end)
+
+RegisterNetEvent("mb-trey:DonutBox", function()
+    local randomToy = math.random(1,10)
+    TriggerServerEvent('QBCore:Server:RemoveItem', "donutbox", 1)
+    TriggerServerEvent('QBCore:Server:AddItem', "chocdonut", 6)
+    TriggerServerEvent('QBCore:Server:AddItem', "pinkdonut", 6)
+
+    if randomToy < 4 then
+        QBCore.Functions.Notify("No toy in box :(", "error")
+    elseif randomToy == 4 then
+        TriggerServerEvent('QBCore:Server:AddItem', "trey-toy1", 1)
+        TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items["trey-toy1"], "add")
+    elseif randomToy < 10 and randomToy > 4 then
+        QBCore.Functions.Notify("No toy in box :(", "error")
+    elseif randomToy == 10 then	
+        TriggerServerEvent('QBCore:Server:AddItem', "trey-toy2", 1)	
+        TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items["trey-toy2"], "add")
+    else	
+        QBCore.Functions.Notify("No toy in box :(", "error")
+    end
 end)
